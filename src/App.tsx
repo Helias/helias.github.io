80b24902677a4e8474212ecb72edd1de72d215b7
Helias <stefanoborzi32@gmail.com>
2025-01-30 12:28:37 +0100

    chore(about): improve about section

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ function App(): JSX.Element {
   return (
     <>
       <Navbar />
-      <img src="Home.jpg" />
+      <div className="relative w-full">
+        <img src="Home.jpg" className="opacity-70 w-full h-[calc(100vh-4rem)] object-cover" />
+        <div className="absolute inset-0 bg-black opacity-40">
+          <div className="mt-[30vh]">
+            <h2 className="ml-[20vw] text-white font-bold">Stefano Borzì</h2>
+            <br />
+            <h3 className="ml-[20vw] text-white font-bold">I am a developer</h3>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
