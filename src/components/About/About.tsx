ad245dc3f4a5d74604c54f37a90ca7f7d374a451
Helias <stefanoborzi32@gmail.com>
2025-02-05 18:11:01 +0100

    chore(about): add google scholar link

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,6 +2,7 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import SchoolIcon from '@mui/icons-material/School';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import SocialLink from './SocialLink';
 
@@ -24,6 +25,9 @@ export default function About(): JSX.Element {
             </SocialLink>
             <SocialLink url="https://linkedin.com/in/stefanoborzi" classNames="text-blue-900">
               <LinkedInIcon sx={{ fontSize: 50 }} />
+            </SocialLink>
+            <SocialLink url="https://scholar.google.com/citations?user=80n8dycAAAAJ&hl=it">
+              <SchoolIcon sx={{ fontSize: 50 }} />
             </SocialLink>
           </div>
           <div className="mt-10 flex items-center justify-center">
