7db15eaab60bd7d065f25b42771581e5775c1bb6
Helias <stefanoborzi32@gmail.com>
2025-02-07 11:03:15 +0100

    chore: update about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -43,8 +43,8 @@ export default function About(): JSX.Element {
           <p>I'm a software engineer who started programming for fun at the age of 12.</p>
           <p className="mt-10">
             I have several years of working experience mostly using web technologies like
-            TypeScript/JavaScript, Angular, Python, Node.js, Bootstrap, HTML, CSS/SCSS, C++, PHP,
-            and Laravel. I also have academic experience with other technologies like Java.
+            TypeScript/JavaScript, Angular, React, Python, Node.js, Bootstrap, HTML, CSS/SCSS, C++,
+            PHP, and Laravel.
           </p>
           <p className="mt-10">
             In my spare time, I manage two opensource communities that I have founded{' '}
